87ef658ec30146a0200aa9914d28680c40edb671
Jefferson Girao <contato@jefferson.eti.br>
2012-05-16 18:53:26 -0300

    First commit, setting up refinery

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VilaPintoWebsite::Application.config.secret_token = '4cc9c72b2189998094fbd92b702f24c583b944c46f39117b71403aebc7310e518f7eee95b7e0a7cb2f143f63c2dc0124eadb949e744016df2c30011b4a9cfeec'
